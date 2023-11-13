7f802ae89e2a318ab60bf08c1ce6bbe4e55c71e4
Ndzi Nfor <ndzinfor@gmail.com>
2023-11-13 10:15:00 +0100

    Write addBookToLibrary function

--- library.js
+++ library.js
@@ -10,12 +10,13 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
   // Prompt the user for book details
-  const title = prompt("Enter the book title:");
-  const author = prompt("Enter the author name:");
-  const pages = prompt("Enter the number of pages:");
-
+  const title = document.getElementById("b-title").value;
+  const author = document.getElementById("author-name").value;
+  const pages = document.getElementById("num-pages").value;
+  const rid = document.getElementById("read").value;
+  
   // Create a new Book object with the user-provided details
-  const newBook = new Book(title, author, pages);
+  const newBook = new Book(title, author, pages, rid);
 
   // Add the new book to the library array
   myLibrary.push(newBook);
@@ -34,10 +35,9 @@ function displayBooks(Arr){
 const popUpForm = document.getElementById("popUpForm");
 var button = document.getElementById("addBook");
 //Form Pop-Up//
-//button.onclick = () => {window.open('hello!')};//
 
 //button function//
 button.addEventListener("click", function() {
   document.getElementById("popUpForm").style.display = "block";
- 
 });
+addBookToLibrary()
