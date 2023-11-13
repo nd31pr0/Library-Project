6d4bde3679f7665ef40ae10ee22989aa5e76eda0
Ndzi Nfor <ndzinfor@gmail.com>
2023-11-14 00:26:56 +0100

    Make displayBooks functio to show books array on page

--- library.js
+++ library.js
@@ -1,9 +1,4 @@
 const myLibrary = [];
-const book1 = new Book("Book 1", "Author 1", 200, "true");
-const book2 = new Book("Book 2", "Author 2", 300, "true");
-const book3 = new Book("Book 3", "Author 3", 250, "false");
-
-myLibrary.push(book1, book2, book3);
 
 
 function Book(title, author, pages, read) {
@@ -14,6 +9,13 @@ function Book(title, author, pages, read) {
     this.read = read;
   }
 
+const book1 = new Book("Book 1", "Author 1", 200, "true");
+const book2 = new Book("Book 2", "Author 2", 300, "true");
+const book3 = new Book("Book 3", "Author 3", 250, "false");
+  
+myLibrary.push(book1, book2, book3);
+  
+
 function addBookToLibrary() {
   // Prompt the user for book details
   const title = document.getElementById("b-title").value;
@@ -23,6 +25,7 @@ function addBookToLibrary() {
   
   // Create a new Book object with the user-provided details
   const newBook = new Book(title, author, pages, rid);
+  console.log(newBook);
 
   // Add the new book to the library array
   myLibrary.push(newBook);
@@ -73,4 +76,4 @@ button.addEventListener("click", function() {
   document.getElementById("popUpForm").style.display = "block";
 });
 
-document.getElementById('submit').addEventListener('click', addBookToLibrary)
+document.getElementById('submit').addEventListener('click', addBookToLibrary())
