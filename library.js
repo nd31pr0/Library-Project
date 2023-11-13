605d5b1e117ccec048172ef301e03127bc8c86a5
Ndzi Nfor <ndzinfor@gmail.com>
2023-11-13 08:53:00 +0100

    Add popup form to html

--- library.js
+++ library.js
@@ -9,7 +9,18 @@ function Book(title, author, pages, read) {
   }
 
 function addBookToLibrary() {
-  // do stuff here
+  // Prompt the user for book details
+  const title = prompt("Enter the book title:");
+  const author = prompt("Enter the author name:");
+  const pages = prompt("Enter the number of pages:");
+
+  // Create a new Book object with the user-provided details
+  const newBook = new Book(title, author, pages);
+
+  // Add the new book to the library array
+  myLibrary.push(newBook);
+
+  console.log("Book added to library:", newBook);
 }
 
 function displayBooks(Arr){
@@ -17,3 +28,16 @@ function displayBooks(Arr){
         return
     })
 }
+
+
+
+const popUpForm = document.getElementById("popUpForm");
+var button = document.getElementById("addBook");
+//Form Pop-Up//
+//button.onclick = () => {window.open('hello!')};//
+
+//button function//
+button.addEventListener("click", function() {
+  document.getElementById("popUpForm").style.display = "block";
+ 
+});
